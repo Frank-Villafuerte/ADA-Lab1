6620b4caf4dbd2dc301b597f13e5f3c20ba60f6b
Frank Villafuerte <fvillafuerte@unsa.edu.pe>
2026-09-11 13:29:42 -0500

    REFACTOR: implementacion de insertion sort para ordenamiento

--- wSort/main.cpp
+++ wSort/main.cpp
@@ -1,52 +1,58 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <chrono>
+using namespace std;
+void Sort(vector<string>& words)
+{
+    int n = words.size();
 
-int shorterWordLenght(const std::string& word1, const std::string& word2)
-{
-    int lenght1 = word1.length(), lenght2 = word2.length();
-    if (lenght1 < lenght2)
-    {
-        return lenght1;
-    }
-    else
-    {
-        return lenght2;
-    }
-}
-void swap(std::string& nombre1, std::string& nombre2)
-{
-    std::string aux = nombre1;
-    nombre1 = nombre2;
-    nombre2 = aux;
-}
-bool compare(std::string& nombre1, std::string& nombre2)
-{
-    for (int i = 0;i < shorterWordLenght(nombre1, nombre2);i++)
-    {
-        if (nombre1.at(i) > nombre2.at(i))
-        {
-            swap(nombre1, nombre2);
-            return false;
+    for (int i = 1; i < n; i++) {
+        string actual = words[i];
+        int j = i - 1;
+        while (j >= 0 && words[j] > actual) {
+            words[j + 1] = words[j];
+            j--;
         }
-    }
-    return true;
-}
-void sort(std::vector<std::string>& list)
-{
-    for (int i = 0;i < list.size();i++)
-    {
-        
+        words[j + 1] = actual;
     }
 }
+
 int main()
 {
-    std::vector<std::string> nombres={
-        "Mateo", "Lucas", "Leo", "Hugo", "Alejandro", "Daniel", "Manuel", "Pablo", "Álvaro", "Adrián"
+    std::vector<std::string> list1={
+        "Maria","Juan","Jose","Francisco","Pedro","Francisca","Antonio","Manuel","Ana","Joseph","Juana","Miguel","Josefa","Antonia","Joan","Catalina","Martin","Teresa","Rosa","Manuela","Domingo","Isabel","Josepha","Ramon","Diego","Andres","Magdalena","Pere","Anna","Ysabel","Luis","Margarita","Theresa","Margarida","Joaquin","Josef","Vicente","Angela","Jaume","Alonso","Salvador","Fernando","Dolores","Jaime","Agustin","Tomas","Sebastian","Carmen","Antoni","Gregorio","Miquel","Mariana","Cathalina","Lorenzo","Luisa","Mariano","Joana","Santiago","Paula","Gabriel","Nicolas","Bartolome","Rafael","Bernardo","Esteban","Pablo","Felipe","Carlos","Julian","Lucia","Vicenta","Alfonso","Ramona","Angel","Agustina","Thomas","Josep","Joaquina","Benito","Juliana","Petra","Leonor","Clara","Blas","Catharina","Madalena","Narciso","Felix","Simon","Gaspar","Agueda","Francesch","Gregoria","Pau","Marianna","Isidro","Beatriz","Micaela","Rita","Geronimo"
     };
+    std::vector<std::string> list2 = {
+        "Maria","Juan","Jose","Francisco","Pedro","Francisca","Antonio","Manuel","Ana","Joseph","Juana","Miguel","Josefa","Antonia","Joan","Catalina","Martin","Teresa","Rosa","Manuela","Domingo","Isabel","Josepha","Ramon","Diego","Andres","Magdalena","Pere","Anna","Ysabel","Luis","Margarita","Theresa","Margarida","Joaquin","Josef","Vicente","Angela","Jaume","Alonso","Salvador","Fernando","Dolores","Jaime","Agustin","Tomas","Sebastian","Carmen","Antoni","Gregorio","Miquel","Mariana","Cathalina","Lorenzo","Luisa","Mariano","Joana","Santiago","Paula","Gabriel","Nicolas","Bartolome","Rafael","Bernardo","Esteban","Pablo","Felipe","Carlos","Julian","Lucia","Vicenta","Alfonso","Ramona","Angel","Agustina","Thomas","Josep","Joaquina","Benito","Juliana","Petra","Leonor","Clara","Blas","Catharina","Madalena","Narciso","Felix","Simon","Gaspar","Agueda","Francesch","Gregoria","Pau","Marianna","Isidro","Beatriz","Micaela","Rita","Geronimo"
+    };
+   
     
+    chrono::steady_clock::time_point start;
+    chrono::steady_clock::time_point end;
+    chrono::duration<double, milli> time;
+
+    start = chrono::high_resolution_clock::now();
+    Sort(list1);
+    end = chrono::high_resolution_clock::now();
+    time = end - start;
+    cout << "Insertion Sort: " << time.count() << "ms" << std::endl;
+    cout << "=====================================" << std::endl;
+    start = chrono::high_resolution_clock::now();
+    sort(list2.begin(), list2.end());
+    end = chrono::high_resolution_clock::now();
+    time = end - start;
+    cout << "Sort STD: " << time.count() << "ms" << std::endl;
+    cout << "=====================================" << std::endl;
+
+
+    /*for (int i = 0;i < nombres.size();i++)
+    {
+        cout << nombres[i]<<", ";
+    }*/
     //for(int i=0;i<nombres->capacity(), i++)
-    std::cout << nombres.size() << std::endl;
+    //std::cout << nombres.size() << std::endl;
 
     return 1;
 }
